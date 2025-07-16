f13b5b307800ae1b3da505482a62e7648d65ccc3
NashatKNDSU <shottybeambusiness@gmail.com>
2025-07-16 10:08:08 -0500

    #guestbook add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { MusicpageComponent } from './musicpage/musicpage.component';
+import { GuestbookComponent } from './guestbook/guestbook.component';
 
 export const routes: Routes = [
 
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomepageComponent },
     { path: 'music', component: MusicpageComponent },
+    { path: 'guestbook', component: GuestbookComponent}
 ];
